bd74ec352281b2ed205e123af16a2428bc984f6f
harinimandava <harini10989@gmail.com>
2020-04-02 13:49:15 +1300

    Added controller model and view

---
+++ WebApplication2/WebApplication2/Controllers/EmpController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class EmpController : Controller
+    {
+        public IActionResult Index()
+
+        {
+            var details = new WebApplication2.Models.HomePageViewModel.Emp(){ Name = "Harini", Gender = "Female", ID = 101 };
+
+            ViewData["Emp"] = details;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/HomePageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class HomePageViewModel
+    {
+        public class Emp    
+        {
+            public int ID { get; set; }
+            public string Name { get; set; }
+            public string Gender { get; set; }
+
+        }
+    }
+}
